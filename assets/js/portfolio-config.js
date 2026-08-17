26558b16249c38eeed9aa6d175e86411e7eddddc
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-08-17 13:23:22 +0630

    Create portfolio-config.js

---
+++ assets/js/portfolio-config.js
@@ -0,0 +1,24 @@
+"use strict";
+
+window.PORTFOLIO_PAGES = {
+  "index.html": {
+    name: "Part 1",
+    sections: "Sections 11-20",
+    previous: null,
+    next: "part2.html"
+  },
+
+  "part2.html": {
+    name: "Part 2",
+    sections: "Sections 21-40",
+    previous: "index.html",
+    next: "part3.html"
+  },
+
+  "part3.html": {
+    name: "Part 3",
+    sections: "Sections 41-50",
+    previous: "part2.html",
+    next: null
+  }
+};
